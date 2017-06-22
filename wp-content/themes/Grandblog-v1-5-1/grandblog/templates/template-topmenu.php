3153fbbe90ae06b3372dc2464b42ff0b4f09f47c
Shaun Chua <shon3005@gmail.com>
2017-06-22 12:28:37 -0400

    eighth commit

--- wp-content/themes/Grandblog-v1-5-1/grandblog/templates/template-topmenu.php
+++ wp-content/themes/Grandblog-v1-5-1/grandblog/templates/template-topmenu.php
@@ -24,10 +24,10 @@ elseif(is_home())
 	{
 	    $tg_topbar = true;
 	}
-    
+
     global $grandblog_topbar;
     $grandblog_topbar = $tg_topbar;
-    
+
     if(!empty($tg_topbar))
     {
 ?>
@@ -35,13 +35,13 @@ elseif(is_home())
 <!-- Begin top bar -->
 <div class="above_top_bar">
     <div class="page_content_wrapper">
-    
+
     <div class="top_contact_info">
 		<?php
 		    $tg_menu_contact_hours = kirki_get_option('tg_menu_contact_hours');
-		    
+
 		    if(!empty($tg_menu_contact_hours))
-		    {	
+		    {
 		?>
 		    <span id="top_contact_hours"><i class="fa fa-clock-o"></i><?php echo esc_html($tg_menu_contact_hours); ?></span>
 		<?php
@@ -50,7 +50,7 @@ elseif(is_home())
 		<?php
 		    //Display top contact info
 		    $tg_menu_contact_number = kirki_get_option('tg_menu_contact_number');
-		    
+
 		    if(!empty($tg_menu_contact_number))
 		    {
 		?>
@@ -59,18 +59,18 @@ elseif(is_home())
 		    }
 		?>
     </div>
-    	
+
     <?php
     	//Display Top Menu
-    	if ( has_nav_menu( 'top-menu' ) ) 
+    	if ( has_nav_menu( 'top-menu' ) )
 		{
-		    wp_nav_menu( 
-		        	array( 
+		    wp_nav_menu(
+		        	array(
 		        		'menu_id'			=> 'top_menu',
 		        		'menu_class'		=> 'top_nav',
 		        		'theme_location' 	=> 'top-menu',
-		        	) 
-		    ); 
+		        	)
+		    );
 		}
     ?>
     <br class="clear"/>
@@ -86,11 +86,11 @@ elseif(is_home())
     //Get page featured image
     if(has_post_thumbnail($current_page_id, 'full'))
     {
-        $image_id = get_post_thumbnail_id($current_page_id); 
+        $image_id = get_post_thumbnail_id($current_page_id);
         $image_thumb = wp_get_attachment_image_src($image_id, 'full', true);
         $pp_page_bg = $image_thumb[0];
     }
-    
+
    if(!empty($pp_page_bg) && basename($pp_page_bg)=='default.png')
     {
     	$pp_page_bg = '';
@@ -103,7 +103,7 @@ elseif(is_home())
     		//Get Soical Icon
 			get_template_part("/templates/template-socials");
     	?>
-		
+
 		<?php
 			//Check if enable main menu
 			$tg_main_menu = kirki_get_option('tg_main_menu');
@@ -111,39 +111,51 @@ elseif(is_home())
 			{
 			    $tg_main_menu = false;
 			}
-			
+
 			if(!empty($tg_main_menu))
 			{
 		?>
-	        <div id="nav_wrapper">
-	        	<div class="nav_wrapper_inner">
-	        		<div id="menu_border_wrapper">
-	        			<?php 	
-	        				//Check if has custom menu
-	        				if ( has_nav_menu( 'primary-menu' ) ) 
-	    					{
-	    		    		    wp_nav_menu( 
-	    		    		        	array( 
-	    		    		        		'menu_id'			=> 'main_menu',
-	    		    		        		'menu_class'		=> 'nav',
-	    		    		        		'theme_location' 	=> 'primary-menu',
-	    		    		        		'walker' => new Grand_Blog_walker(),
-	    		    		        	) 
-	    		    		    ); 
-	    		    		}
-	    		    		else
-	    		    		{
-	    			    	    echo '<div class="notice">'.esc_html__('Setup Menu via Wordpress Dashboard > Appearance > Menus', 'grandblog-translation' ).'</div>';
-	    		    		}
-	        			?>
-	        		</div>
-	        	</div>
-	        </div>
+            <nav class="navbar navbar-fixed-top" style="background-color: white;">
+                <div class="container max-width-900 wi-navbar" style="height: 73px;">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" #toggler>
+                            <span>
+                                <img src="http://cdn.appwinit.com/small-menu/image.png" alt="menu" srcset="http://cdn.appwinit.com/small-menu/image@2x.png 2x, http://cdn.appwinit.com/small-menu/image@3x.png 3x" style="height: 19px; width: 25px;margin-top: 3px;"/>
+                            </span>
+                        </button>
+                        <a class="navbar-brand" href="http://www.appwinit.com/"><img src="http://cdn.appwinit.com/logo/image.png" alt="Logo" srcset="http://cdn.appwinit.com/logo/image@2x.png 2x, http://cdn.appwinit.com/logo/image@3x.png 3x" style="height: 22px; width: 77px;"/></a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right desktop">
+                            <li><a href="http://www.appwinit.com/">Home</a></li>
+                            <li><a href="http://www.appwinit.com/how-it-works">How it works</a></li>
+                            <li><a href="http://www.appwinit.com/business">Business</a></li>
+                            <li><a href="http://www.appwinit.com/faq/parking-tickets">FAQ</a></li>
+                            <li><a href="http://www.appwinit.com/contact">Contact</a></li>
+                            <li><a href="http://blog.appwinit.com">Blog</a></li>
+                            <!-- <li *ngIf="!userService.authenticated"><a routerLink="/log-in" routerLinkActive="active">Log in</a></li> -->
+                            <!-- <li *ngIf="!userService.authenticated"><a class="bordered  sign-up-button" routerLink="/sign-up" routerLinkActive="active">Sign Up</a></li> -->
+                        </ul>
+
+                        <ul class="nav navbar-nav navbar-right mobile">
+                            <li><a href="http://www.appwinit.com/">Home</a></li>
+                            <li><a href="http://www.appwinit.com/how-it-works">How it works</a></li>
+                            <li><a href="http://www.appwinit.com/business">Business</a></li>
+                            <li><a href="http://www.appwinit.com/faq/parking-tickets">FAQ</a></li>
+                            <li><a href="http://www.appwinit.com/contact">Contact</a></li>
+                            <li><a href="http://blog.appwinit.com">Blog</a></li>
+                            <!-- <li *ngIf="!userService.authenticated"><a routerLink="/log-in" routerLinkActive="active">Log in</a></li> -->
+                            <!-- <li *ngIf="!userService.authenticated"><a class="bordered  sign-up-button" routerLink="/sign-up" routerLinkActive="active">Sign Up</a></li> -->
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 	        <!-- End main nav -->
         <?php
         	}
         ?>
-        
+
         <!-- Begin right corner buttons -->
     	<div id="logo_right_button">
     		<?php
@@ -156,36 +168,36 @@ elseif(is_home())
 			<?php
 				}
 			?>
-			
+
 			<!-- Begin search icon -->
 			<a href="javascript:;" id="search_icon"><i class="fa fa-search"></i></a>
 			<!-- End side menu -->
-    	
+
 	    	<!-- Begin search icon -->
 			<a href="javascript:;" id="mobile_nav_icon"></a>
 			<!-- End side menu -->
-			
+
     	</div>
     	<!-- End right corner buttons -->
         </div>
     	</div>
     </div>
-    
+
     <!-- Begin logo -->
     <div id="logo_wrapper">
-    
+
     <?php
         //get custom logo
         $tg_retina_logo = kirki_get_option('tg_retina_logo');
 
         if(!empty($tg_retina_logo))
-        {	
+        {
         	//Get image width and height
 	    	$image_id = grandblog_get_image_id($tg_retina_logo);
 	    	$obj_image = wp_get_attachment_image_src($image_id, 'original');
 	    	$image_width = 0;
 	    	$image_height = 0;
-	    	
+
 	    	if(isset($obj_image[1]))
 	    	{
 	    		$image_width = intval($obj_image[1]/2);
@@ -209,7 +221,7 @@ elseif(is_home())
 	    			{
 	    		?>
 	        	<img src="<?php echo esc_url($tg_retina_logo); ?>" alt="<?php esc_attr(get_bloginfo('name')); ?>" width="252" height="108"/>
-	        	<?php 
+	        	<?php
 	    	    	}
 	    	    ?>
 	        </a>
