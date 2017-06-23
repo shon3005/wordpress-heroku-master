e187187fef4ec976193fe7aa452f89b7b17a5e8f
Shaun Chua <shon3005@gmail.com>
2017-06-23 10:40:37 -0400

    l

--- wp-content/themes/Grandblog-v1-5-1/grandblog/templates/template-topmenu.php
+++ wp-content/themes/Grandblog-v1-5-1/grandblog/templates/template-topmenu.php
@@ -75,7 +75,7 @@ elseif(is_home())
     ?>
     <br class="clear"/>
     </div>
-<!-- </div> -->
+</div>
 <?php
     }
 ?>
@@ -99,53 +99,7 @@ elseif(is_home())
 <!-- <div class="top_bar"> -->
 
 		<!-- <div id="menu_wrapper"> -->
-        <nav class="navbar navbar-fixed-top" style="background-color: white;">
-            <div class="container max-width-900 wi-navbar" style="height: 73px;">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" #toggler>
-                        <span>
-                            <img src="http://cdn.appwinit.com/small-menu/image.png" alt="menu" srcset="http://cdn.appwinit.com/small-menu/image@2x.png 2x, http://cdn.appwinit.com/small-menu/image@3x.png 3x" style="height: 19px; width: 25px;margin-top: 3px;"/>
-                        </span>
-                    </button>
-                    <a class="navbar-brand" href="http://www.appwinit.com/"><img src="http://cdn.appwinit.com/logo/image.png" alt="Logo" srcset="http://cdn.appwinit.com/logo/image@2x.png 2x, http://cdn.appwinit.com/logo/image@3x.png 3x" style="height: 22px; width: 77px;"/></a>
-                </div>
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right desktop">
-                        <li><a href="http://www.appwinit.com/">Home</a></li>
-                        <li><a href="http://www.appwinit.com/how-it-works">How it works</a></li>
-                        <li><a href="http://www.appwinit.com/business">Business</a></li>
-                        <li><a href="http://www.appwinit.com/faq/parking-tickets">FAQ</a></li>
-                        <li><a href="http://www.appwinit.com/contact">Contact</a></li>
-                        <li><a href="http://blog.appwinit.com">Blog</a></li>
-                        <!-- <li *ngIf="!userService.authenticated"><a routerLink="/log-in" routerLinkActive="active">Log in</a></li> -->
-                        <!-- <li *ngIf="!userService.authenticated"><a class="bordered  sign-up-button" routerLink="/sign-up" routerLinkActive="active">Sign Up</a></li> -->
-                    </ul>
-
-                    <?php
-                    wp_nav_menu( array(
-                    'theme_location' => 'top_menu',
-                    'depth' => 2,
-                    'container' => false,
-                    'menu_class' => 'nav navbar-nav',
-                    'fallback_cb' => 'wp_page_menu',
-                    //Process nav menu using our custom nav walker
-                    'walker' => new wp_bootstrap_navwalker())
-                    );
-                    ?>
-                    <!-- <ul class="nav navbar-nav navbar-right mobile"> -->
-                        <!-- <li><a href="http://www.appwinit.com/">Home</a></li> -->
-                        <!-- <li><a href="http://www.appwinit.com/how-it-works">How it works</a></li> -->
-                        <!-- <li><a href="http://www.appwinit.com/business">Business</a></li> -->
-                        <!-- <li><a href="http://www.appwinit.com/faq/parking-tickets">FAQ</a></li> -->
-                        <!-- <li><a href="http://www.appwinit.com/contact">Contact</a></li> -->
-                        <!-- <li><a href="http://blog.appwinit.com">Blog</a></li> -->
-                        <!-- <li *ngIf="!userService.authenticated"><a routerLink="/log-in" routerLinkActive="active">Log in</a></li> -->
-                        <!-- <li *ngIf="!userService.authenticated"><a class="bordered  sign-up-button" routerLink="/sign-up" routerLinkActive="active">Sign Up</a></li> -->
-                    <!-- </ul> -->
-                </div>
-            </div>
-        </nav>
 
 		<?php
 			//Check if enable main menu
@@ -158,10 +112,95 @@ elseif(is_home())
 			if(!empty($tg_main_menu))
 			{
 		?>
+            <nav class="navbar navbar-fixed-top" style="background-color: white;">
+                <div class="container max-width-900 wi-navbar" style="height: 73px;">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" #toggler>
+                            <span>
+                                <img src="http://cdn.appwinit.com/small-menu/image.png" alt="menu" srcset="http://cdn.appwinit.com/small-menu/image@2x.png 2x, http://cdn.appwinit.com/small-menu/image@3x.png 3x" style="height: 19px; width: 25px;margin-top: 3px;"/>
+                            </span>
+                        </button>
+                        <a class="navbar-brand" href="http://www.appwinit.com/"><img src="http://cdn.appwinit.com/logo/image.png" alt="Logo" srcset="http://cdn.appwinit.com/logo/image@2x.png 2x, http://cdn.appwinit.com/logo/image@3x.png 3x" style="height: 22px; width: 77px;"/></a>
+                    </div>
 
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right desktop">
+                            <li><a href="http://www.appwinit.com/">Home</a></li>
+                            <li><a href="http://www.appwinit.com/how-it-works">How it works</a></li>
+                            <li><a href="http://www.appwinit.com/business">Business</a></li>
+                            <li><a href="http://www.appwinit.com/faq/parking-tickets">FAQ</a></li>
+                            <li><a href="http://www.appwinit.com/contact">Contact</a></li>
+                            <li><a href="http://blog.appwinit.com">Blog</a></li>
+                            <!-- <li *ngIf="!userService.authenticated"><a routerLink="/log-in" routerLinkActive="active">Log in</a></li> -->
+                            <!-- <li *ngIf="!userService.authenticated"><a class="bordered  sign-up-button" routerLink="/sign-up" routerLinkActive="active">Sign Up</a></li> -->
+                        </ul>
+
+                        <ul class="nav navbar-nav navbar-right mobile">
+                            <li><a href="http://www.appwinit.com/">Home</a></li>
+                            <li><a href="http://www.appwinit.com/how-it-works">How it works</a></li>
+                            <li><a href="http://www.appwinit.com/business">Business</a></li>
+                            <li><a href="http://www.appwinit.com/faq/parking-tickets">FAQ</a></li>
+                            <li><a href="http://www.appwinit.com/contact">Contact</a></li>
+                            <li><a href="http://blog.appwinit.com">Blog</a></li>
+                            <!-- <li *ngIf="!userService.authenticated"><a routerLink="/log-in" routerLinkActive="active">Log in</a></li> -->
+                            <!-- <li *ngIf="!userService.authenticated"><a class="bordered  sign-up-button" routerLink="/sign-up" routerLinkActive="active">Sign Up</a></li> -->
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         <?php
         	}
         ?>
         <!-- </div> -->
     	<!-- </div> -->
     </div>
+
+    <!-- Begin logo -->
+    <div id="logo_wrapper">
+
+    <?php
+        //get custom logo
+        $tg_retina_logo = kirki_get_option('tg_retina_logo');
+
+        if(!empty($tg_retina_logo))
+        {
+        	//Get image width and height
+	    	$image_id = grandblog_get_image_id($tg_retina_logo);
+	    	$obj_image = wp_get_attachment_image_src($image_id, 'original');
+	    	$image_width = 0;
+	    	$image_height = 0;
+
+	    	if(isset($obj_image[1]))
+	    	{
+	    		$image_width = intval($obj_image[1]/2);
+	    	}
+	    	if(isset($obj_image[2]))
+	    	{
+	    		$image_height = intval($obj_image[2]/2);
+	    	}
+    ?>
+    <div id="logo_normal" class="logo_container">
+        <div class="logo_align">
+	        <a id="custom_logo" class="logo_wrapper default" href="<?php echo esc_url(home_url('/')); ?>">
+	        	<?php
+	    			if($image_width > 0 && $image_height > 0)
+	    			{
+	    		?>
+	    		<img src="<?php echo esc_url($tg_retina_logo); ?>" alt="<?php esc_attr(get_bloginfo('name')); ?>" width="<?php echo esc_attr($image_width); ?>" height="<?php echo esc_attr($image_height); ?>"/>
+	    		<?php
+	    			}
+	    			else
+	    			{
+	    		?>
+	        	<img src="<?php echo esc_url($tg_retina_logo); ?>" alt="<?php esc_attr(get_bloginfo('name')); ?>" width="252" height="108"/>
+	        	<?php
+	    	    	}
+	    	    ?>
+	        </a>
+        </div>
+    </div>
+    <?php
+        }
+    ?>
+    <!-- End logo -->
+</div>
