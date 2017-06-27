fcc497b916e2d84b395060fe47a50a315a90f6b1
Shaun Chua <shon3005@gmail.com>
2017-06-27 15:43:34 -0400

    l

--- wp-content/themes/grandblog/header.php
+++ wp-content/themes/grandblog/header.php
@@ -4,13 +4,13 @@
  *
  * @package WordPress
  */
- 
+
 if (!isset( $content_width ) ) $content_width = 1170;
 
 if(session_id() == '') {
 	session_start();
 }
- 
+
 global $grandblog_homepage_style;
 
 ?><!DOCTYPE html>
@@ -22,13 +22,13 @@ global $grandblog_homepage_style;
 
 <?php
 	//Fallback compatibility for favicon
-	if(!function_exists( 'has_site_icon' ) || ! has_site_icon() ) 
+	if(!function_exists( 'has_site_icon' ) || ! has_site_icon() )
 	{
 		/**
 		*	Get favicon URL
 		**/
 		$tg_favicon = kirki_get_option('tg_favicon');
-		
+
 		if(!empty($tg_favicon))
 		{
 ?>
@@ -36,7 +36,7 @@ global $grandblog_homepage_style;
 <?php
 		}
 	}
-?> 
+?>
 
 <?php
 	/* Always have wp_head() just before the closing </head>
@@ -53,29 +53,29 @@ global $grandblog_homepage_style;
 	<?php
 		//Check if disable right click
 		$tg_enable_right_click = kirki_get_option('tg_enable_right_click');
-		
+
 		//Check if disable image dragging
 		$tg_enable_dragging = kirki_get_option('tg_enable_dragging');
-		
+
 		//Check if use AJAX search
 		$tg_menu_search_instant = kirki_get_option('tg_menu_search_instant');
-		
+
 		//Check if sticky menu
 		$tg_fixed_menu = kirki_get_option('tg_fixed_menu');
-		
+
 		//Check if display top bar
 		$tg_topbar = kirki_get_option('tg_topbar');
 		if(THEMEDEMO && isset($_GET['topbar']) && !empty($_GET['topbar']))
 		{
 			$tg_topbar = true;
 		}
-		
+
 		//Check if add blur effect
 		$tg_page_title_img_blur = kirki_get_option('tg_page_title_img_blur');
-		
+
 		//Check filterable link option
 		$tg_portfolio_filterable_link = kirki_get_option('tg_portfolio_filterable_link');
-		
+
 		//Check slider layout
 		$tg_blog_slider_layout = kirki_get_option('tg_blog_slider_layout');
 		if(THEMEDEMO && isset($_GET['layout']) && ($_GET['layout'] == 'fullwidth' OR $_GET['layout'] == 'full_fullwidth'))
@@ -93,79 +93,18 @@ global $grandblog_homepage_style;
 	<input type="hidden" id="pp_page_title_img_blur" name="pp_page_title_img_blur" value="<?php echo esc_attr($tg_page_title_img_blur); ?>"/>
 	<input type="hidden" id="tg_blog_slider_layout" name="tg_blog_slider_layout" value="<?php echo esc_attr($tg_blog_slider_layout); ?>"/>
 	<input type="hidden" id="pp_back" name="pp_back" value="<?php esc_html_e( 'Back', 'grandblog-translation' ); ?>"/>
-	
+
 	<?php
 		//Check footer sidebar columns
 		$tg_footer_sidebar = kirki_get_option('tg_footer_sidebar');
 	?>
 	<input type="hidden" id="pp_footer_style" name="pp_footer_style" value="<?php echo esc_attr($tg_footer_sidebar); ?>"/>
-	
-	<!-- Begin mobile menu -->
-	<a id="close_mobile_menu" href="javascript:;"></a>
-	<div class="mobile_menu_wrapper">
-		<?php
-    	    //Check if display search in header	
-    	    $tg_menu_search = kirki_get_option('tg_menu_search');
-    	    
-    	    if(!empty($tg_menu_search))
-    	    {
-    	?>
-    	<form role="search" method="get" name="searchform" id="searchform" action="<?php echo esc_url(home_url('/')); ?>/">
-    	    <div>
-    	    	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" autocomplete="off" placeholder="<?php esc_html_e( 'Search...', 'grandblog-translation' ); ?>"/>
-    	    	<button>
-    	        	<i class="fa fa-search"></i>
-    	        </button>
-    	    </div>
-    	    <div id="autocomplete"></div>
-    	</form>
-    	<?php
-    	    }
-    	?>
-    	
-	    <?php 
-	    	//Check if has custom menu
-			if(is_object($post) && $post->post_type == 'page')
-			{
-			    $page_menu = get_post_meta($post->ID, 'page_menu', true);
-			}	
-			
-			if ( has_nav_menu( 'side-menu' ) ) 
-			{
-			    //Get page nav
-			    wp_nav_menu( 
-			        array( 
-			            'menu_id'			=> 'mobile_main_menu',
-		                'menu_class'		=> 'mobile_main_nav',
-			            'theme_location' 	=> 'side-menu',
-			        )
-			    ); 
-			}
-		?>
-		
-		<!-- Begin side menu sidebar -->
-		<div class="page_content_wrapper">
-			<div class="sidebar_wrapper">
-		        <div class="sidebar">
-		        
-		        	<div class="content">
-		        
-		        		<ul class="sidebar_widget">
-		        		<?php dynamic_sidebar('Side Menu Sidebar'); ?>
-		        		</ul>
-		        	
-		        	</div>
-		    
-		        </div>
-			</div>
-		</div>
-		<!-- End side menu sidebar -->
-	</div>
-	<!-- End mobile menu -->
+
+
 
 	<!-- Begin template wrapper -->
 	<div id="wrapper">
-	
+
 	<?php
 	    //Get main menu layout
 		get_template_part("/templates/template-topmenu");
